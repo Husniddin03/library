c6e6592138b0b9500286c4a2a18f3c6161e32ad1
Husniddin03 <husniddin13124041@gmail.com>
2025-05-20 22:36:23 +0500

    Add new views for books, contact, and user management
    - Created show.blade.php for displaying book details with a responsive layout and integrated styles.
    - Added contact.blade.php for the contact page, including a contact form and Google Maps integration.
    - Implemented user management views: create.blade.php, edit.blade.php, index.blade.php, and show.blade.php with placeholder content.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+Route::resource('authors', AuthorController::class);
+Route::resource('books', BookController::class);
+Route::resource('users', UserController::class);
